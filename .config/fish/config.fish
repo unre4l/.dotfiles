c0e6336768310894536ec6ee4b83fcb7eea64efe
Jonathan Wischhusen <j.wischhusen@demv.de>
2020-04-09 11:41:20 +0200

    add files

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+#set -gx PATH ~/bin $PATH
+
+if status is-interactive
+and not set -q TMUX
+    set -g TMUX tmux new-session -d -s 🏄
+    eval $TMUX
+    tmux attach-session -d -t 🏄
+end
+
+set fish_greeting
+set SPACEFISH_PROMPT_ORDER time user dir host git exec_time line_sep jobs exit_code char
+alias dup 'docker-compose up -d'
+
+alias e 'nautilus'
+alias gs 'git status'
+
+function gbda -d "Delete all branches merged in current HEAD"
+  git branch --merged | \
+    command grep -vE  '^\*|^\s*(master|develop)\s*$' | \
+    command xargs -n 1 git branch -d
+end
+
